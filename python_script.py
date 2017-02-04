1454a90fcd26dc719d729f549d7e4897c5be54fe
Marv Marvelous <tommarv@gmail.com>
2017-02-04 09:14:14 +0100

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -17,15 +17,6 @@ param2 = "9600"
 ard = serial.Serial(param1, param2, timeout=.1)
 a=55
 stream = io.BytesIO()
-import subprocess
-from cStringIO import StringIO
-id_card = []
-mylist = []
-param1 = "/dev/ttyUSB0"
-param2 = "9600"
-ard = serial.Serial(param1, param2, timeout=.1)
-a=55
-stream = io.BytesIO()
 while (a>1):
         a=a-1
         ard.flushInput()
@@ -36,39 +27,20 @@ while (a>1):
                 today = time.strftime("%Y-%m-%d %H:%M:%S")
                 db = mdb.connect("localhost","root","Paulinka94","doch_sys")
                 cursor = db.cursor()
-        a=a-1
-        ard.flushInput()
-        ard.flushOutput()
-        id_card = (ard.readline()).split(",")
-        if (id_card[0]<>''):
-                print id_card[0]
-                today = time.strftime("%Y-%m-%d %H:%M:%S")
-                db = mdb.connect("localhost","root","Paulinka94","doch_sys")
-                cursor = db.cursor()
-                sql = "INSERT INTO rfid_vchod(id_card,time_date) VALUES('"
+                sql = "INSERT INTO rfid_citacka(id_card,time_date) VALUES('"
                 sql = sql+id_card[0]+"',"
                 sql = sql+"'"+today
                 sql = sql+"')"
                 print sql
                 cursor.execute(sql)
-                db.commit()
+                db.commit()     
                 db.rollback()
                 db.close()
                 ard.write("OK\n")
-                sql = sql+id_card[0]+"',"
-                sql = sql+"'"+today
-                sql = sql+"')"
-                print sql
-                cursor.execute(sql)
-                db.commit()
-                db.rollback()
-                db.close()
-                ard.write("OK\n")
-                today = time.strftime("%Y-%m-%d %H:%M:%S")
                 with picamera.PiCamera() as camera:
                         camera.resolution = (1600, 1200)
                         camera.rotation = 90
                         camera.annotate_text = today
                         camera.capture('image'+str(today)+'.jpeg')
         else:
-                time.sleep(1);
+                time.sleep(1)
